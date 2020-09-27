5021582ec3126174089b9db79d8bc768305c5eda
Adam Siegel <tobioatom@gmail.com>
2020-09-27 15:50:50 -0700

    enabling env var logging selection

--- logger.go
+++ logger.go
@@ -1,11 +1,14 @@
 package i2c
 
-import logger "github.com/d2r2/go-logger"
+import (
+	"os"
+	"strconv"
+
+	logger "github.com/d2r2/go-logger"
+)
 
 // You can manage verbosity of log output
 // in the package by changing last parameter value
 // (comment/uncomment corresponding lines).
-var lg = logger.NewPackageLogger("i2c",
-	// logger.DebugLevel,
-	logger.InfoLevel,
-)
+var logLevel, err = strconv.Atoi(os.Getenv("GO_I2C_LOG"))
+var lg = logger.NewPackageLogger("i2c", logLevel)
